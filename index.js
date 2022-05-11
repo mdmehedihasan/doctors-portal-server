350bbbc44f047a32e908a7dd3548605781c6c2be
mehedi hasan <mehedicsebu@gmail.com>
2022-05-11 20:27:43 +0600

    data load from mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -9,9 +9,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ay8jp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ay8jp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
