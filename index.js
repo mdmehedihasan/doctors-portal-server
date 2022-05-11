bb2f72bf7ef125f3eb540df07f7d345026c2c2ce
mehedi hasan <mehedicsebu@gmail.com>
2022-05-11 19:43:41 +0600

    dot env file name correction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-require('dotevn').config();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
