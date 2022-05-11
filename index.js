06df2294cf35d800b23c275680e58c7d83adc366
mehedi hasan <mehedicsebu@gmail.com>
2022-05-11 19:41:19 +0600

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const cors = require('cors');
 require('dotevn').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 //middleware set up
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ay8jp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from doctor!')
